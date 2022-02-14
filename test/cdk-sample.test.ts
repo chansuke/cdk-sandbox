968222542e361e9dffb7decbd23da0e75edf5251
chansuke <moonset20@gmail.com>
2022-02-15 03:17:51 +0900

    feat: simple unit test

--- test/cdk-sample.test.ts
+++ test/cdk-sample.test.ts
@@ -1,17 +1,22 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CdkSample from '../lib/cdk-sample-stack';
+import { Template  } from "aws-cdk-lib/assertions";
+import * as cdk from "aws-cdk-lib";
+import * as ActiveTimer from '../lib/cdk-sample-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-sample-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new CdkSample.CdkSampleStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe('testing VPC', () => {
+  test('vpc resource check', () => {
+    const app = new cdk.App();
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
-});
+
+    const stack = new ActiveTimer.CdkSampleStack(app, 'ActiveTimerStack');
+
+    // Prepare the stack for assertions.
+    // https://docs.aws.amazon.com/ja_jp/cdk/v2/guide/testing.html
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::EC2::VPC', {
+      CidrBlock: '10.0.0.0/16',
+      Tags: [{ 'Key': 'Name', 'Value': 'activetimer-stg-vpc' }]
+    })
+    template.resourceCountIs('AWS::EC2::VPC', 1);
+  })
+})
